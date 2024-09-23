b322f697b899d848aa90049043b08676628d99f4
MitchCas <155246406+Mitchellcass@users.noreply.github.com>
2024-09-22 23:22:51 -0400

    Create placeOrder function

--- cc_5.js
+++ cc_5.js
@@ -1,4 +1,4 @@
-//Create an Inventory Array of Product Objects
+// Task 1: Create an Inventory Array of Product Objects
 
 const inventory = [
     { name: 'Coffee', price: 4, quantity: 15 },
@@ -7,8 +7,44 @@ const inventory = [
     { name: 'Muffin', price: 3, quantity: 30 }
   ];
   
-//Create an Orders Array of Order Objects
+
+// Task 2: Create an Orders Array of Order Objects
 
 const orders = [];
 
 
+// Task 3: Create a Function to Place an Order
+
+function placeOrder(customerName, items) {
+    // Check item stock
+        for (let orderedItem of items) {
+          let inventoryItem = inventory.find(product => product.name === orderedItem.name);
+        
+    // Check ordered item existence within inventory
+          if (!inventoryItem) {
+            console.log(`Error: ${orderedItem.name} does not exist in inventory.`);
+            return;
+          }
+        
+    // Check for enough stock of ordered item
+          if (orderedItem.quantity > inventoryItem.quantity) {
+            console.log(`Error: Not enough ${orderedItem.name} in stock.`);
+            return; 
+          }
+        }
+        
+    // Update inventory and add order if item(s) in stock.
+        for (let orderedItem of items) {
+          let inventoryItem = inventory.find(product => product.name === orderedItem.name);
+          inventoryItem.quantity -= orderedItem.quantity;
+        }
+        
+    // Create order and push to orders array
+        orders.push({
+            customerName, items, status: 'Pending' });
+        console.log(`Order placed for ${customerName}.`)};
+    
+      
+    // Example order
+      placeOrder('Bob', [{ name: 'Frappuccino', quantity: 1 }]);
+    

842a9f975a5f5ccf174efa087195a6452f17b19b
MitchCas <155246406+Mitchellcass@users.noreply.github.com>
2024-09-22 22:02:27 -0400

    Initialize inventory with product objects

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+//Create an Inventory Array of Product Objects
+
+const inventory = [
+    { name: 'Coffee', price: 4, quantity: 15 },
+    { name: 'Croissant', price: 3, quantity: 30 },
+    { name: 'Frappuccino', price: 5, quantity: 15 },
+    { name: 'Muffin', price: 3, quantity: 30 }
+  ];
+  
+  
